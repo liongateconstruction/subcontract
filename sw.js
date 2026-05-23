067e16a8bba10015a7295d2c262e9a1b69481720
liongateconstruction <hegaidima7@gmail.com>
2026-05-23 05:02:05 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'liongate-v73';
+const CACHE = 'liongate-v76';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
