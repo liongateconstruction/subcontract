8c31fbf60780efc4ea737c94b310c42cd397a363
liongateconstruction <hegaidima7@gmail.com>
2026-05-31 00:13:09 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='liongate-v114-coachui';
+const CACHE='liongate-v115-aistyle';
 const ASSETS=['./','./index.html','./manifest.json','./icon192.png','./icon512.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return Promise.all(ASSETS.map(function(a){return c.add(a).catch(function(){});}));}).catch(function(){}));self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==CACHE;}).map(function(x){return caches.delete(x);}));}).then(function(){return self.clients.claim();}));});
